e97e12ab33b51e2d71e2ec94a136c0cc908c1f08
choccocookie <Karanovvlad@gmail.com>
2024-07-19 23:54:27 +0300

    Настроена админка согласно заданию: Для моделей категории и продукта настройте отображение в административной панели. Для категорий выведите id и наименование в список отображения, а для продуктов выведите в список id, название, цену и категорию.
    При этом интерфейс вывода продуктов настройте так, чтобы можно было результат отображения фильтровать по категории, а также осуществлять поиск по названию и полю описания.

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from catalog.models import Product, Category
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product_name', 'category', 'purchase_price')
+    list_filter = ('category',)
+    search_fields = ('product_name', 'product_description',)
+
+@admin.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category_name')
+
+
