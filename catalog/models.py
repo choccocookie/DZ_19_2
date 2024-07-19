ed574462768d5ddf291391a8998f365da8da6199
choccocookie <Karanovvlad@gmail.com>
2024-07-19 22:58:31 +0300

    Добавлена модель Product c начальными настройками Meta, __str__ Добавлены настройки database, media_url, media_root

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=50, verbose_name='Название продукта', help_text='Введите название продукта')
+    product_description = models.TextField(verbose_name='Описание продукта', help_text='Опишите продукт', blank=True, null=True)
+    image = models.IntegerField(upload_to='catalog/photo', blank=True, null=True, help_text='Загрузите фото продукта')
+    category = models.CharField(max_length=50, verbose_name='Категория продукта', help_text='Введите категорию продукта')
+    purchase_price = models.CharField(max_length=30, verbose_name='Цена продукта', help_text='Введите цену продукта')
+    created_at = models.DateField(verbose_name='Дата создания продукта', help_text='Введите дату создания продукта', blank=True, null=True)
+    updated_at = models.DateField(verbose_name='Дата изменения продукта', help_text='Введите дату изменения продукта', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ['product_name', 'category', 'purchase_price']
+
+    def __str__(self):
+        return self.product_name
+
+
+
+
+class Category(models.Model):
+    сategory_name = models.CharField(max_length=50, verbose_name='Название категории', help_text='Введите название категории')
+    сategory_description = models.TextField(verbose_name='Описание категории', help_text='Опишите категорию', blank=True, null=True)
+
+
--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'orm',
+        'USER': 'postgres',
+        'PASSWORD': 'sky.pro'
     }
 }
 
@@ -125,3 +127,6 @@ STATICFILES_DIRS = (BASE_DIR / 'static',)
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/photo/'
+MEDIA_ROOT = BASE_DIR / 'photo'
