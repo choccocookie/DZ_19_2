09a048476daa6d8450fbdfb4d2f97ed0f78ffee9
choccocookie <Karanovvlad@gmail.com>
2024-07-23 19:47:11 +0300

    ДОбавлены js, css Изменено расположение директории для фото продуктов

--- catalog/models.py
+++ catalog/models.py
@@ -36,7 +36,7 @@ class Product(models.Model):
         null=True,
     )
     image = models.ImageField(
-        upload_to="catalog/photo",
+        upload_to="products/photo",
         blank=True,
         null=True,
         help_text="Загрузите фото продукта",
