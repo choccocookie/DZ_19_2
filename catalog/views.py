c83716a9b8c7cc818b023a991bf820811bc8716a
choccocookie <Karanovvlad@gmail.com>
2024-08-18 14:03:18 +0300

    Добалена инфа о версии продукта

--- catalog/views.py
+++ catalog/views.py
@@ -17,21 +17,23 @@ def save_feedback_to_file(name, email, message, filename='feedback.txt'):
 
 class ProductListView(ListView):
     model = Product
-    # template_name = 'catalog/product_list.html'
-    # сontext_object_name = 'product'
-    #
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #
-    #     # Получаем все продукты
-    #     products = context['product']
-    #
-    #     # Для каждого продукта выбираем текущую (активную) версию
-    #     for product in products:
-    #         active_version = product.versions.filter(is_current=True).first()
-    #         product.active_version = active_version
-    #
-    #     return context
+    template_name = 'catalog/product_list.html'
+    context_object_name = 'products'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Получаем все продукты
+        products = context['products']
+
+        # Для каждого продукта выбираем текущую (активную) версию
+        for product in products:
+            active_version = product.versions.filter(version_attribute=True).first()
+            product.active_version = active_version
+
+        return context
+
+
 
 # def home(request):
 #     latest_products = Product.objects.all().order_by('-created_at')[:5]
@@ -104,13 +106,6 @@ class ProductUpdateView(UpdateView):
             context_data['formset'] = ProductFormset(self.request.POST, instance=self.object)
         else:
             context_data['formset'] = ProductFormset(instance=self.object)
-
-
-        # Добавление активной версии для текущего продукта
-        active_version = self.object.versions.filter(is_current=True).first()
-        if active_version:
-            context_data['active_version'] = active_version
-
         return context_data
 
     def form_valid(self, form):
